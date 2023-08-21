d0b07c647f6132dad100597a3500f7e7a9bbae29
David-Rodrigues-Mendes <David_g.mendes@hotmail.com>
2023-08-21 07:12:56 +0100

    Add new fields to album model table

--- db/migrate/20230810103259_create_albums.rb
+++ db/migrate/20230810103259_create_albums.rb
@@ -2,6 +2,9 @@ class CreateAlbums < ActiveRecord::Migration[7.0]
   def change
     create_table :albums do |t|
       t.bigint :album_id, null: false, unique: true, index: true
+      t.string :title, null: false
+      t.string :subtitle
+      t.string :thumbnail
       t.boolean :favorite, default: false, null: false
       
       t.timestamps
